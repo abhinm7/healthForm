4f66b7997a1ebeb8ed5dc1a828299953bef51932
ABHIN M <abhin18m@gmail.com>
2025-12-01 20:53:25 +0530

    feat: complete responsive all screen size

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Example for React
+
+// vitejs.dev
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, 
+  },
+});
